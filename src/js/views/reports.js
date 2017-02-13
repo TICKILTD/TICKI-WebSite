68440b3b15352416e5f4ea9a39d40a7b3d0cd50b
siparsons <simon.parsons@retig.co.uk>
2017-02-13 10:37:46 +0000

    added message aroudn trial period ending

--- src/js/views/reports.js
+++ src/js/views/reports.js
@@ -1,11 +1,53 @@
 import $ from 'jquery'
 import moment from 'moment'
+import ko from 'knockout'
+import axios from 'axios'
 
 import "az-styles";
 import "bootstrap";
 
 import "../functions.js";
 
-$(document).ready(function () {
-});
+var SubmisisionReportLineViewModel = function(clientId, submission) {
+    var self = this;
 
+    self.firstName = ko.observable(submission.person.firstName);
+    self.lastName = ko.observable(submission.person.lastname);
+    self.email = ko.observable(submission.person.email);
+    self.accountId = ko.observable(submission.person.externId);
+    self.submissionId = ko.observable(submission.token);
+    self.clientId = ko.observable(clientId);
+    
+    self.hostedPageUrl = ko.computed(function() {
+        return 'http://localhost:3000/submission?cid=' + self.clientId() + "&sid=" + self.submissionId();
+    })
+}
+
+var SubmissionsReportViewModel = function(clientId) {
+    
+    var self = this;
+
+    self.clientId = ko.observable(clientId);
+    self.results = ko.observableArray([]);
+
+    self.init = function() {
+        axios
+            .get('http://localhost:8080/api/tenants/' + self.clientId() + '/submissions')
+            .then(function (response) {
+                
+                for (var submission of response.data) {
+                    self.results.push(new SubmisisionReportLineViewModel(self.clientId(), submission));
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    self.init();
+}
+
+$(document).ready(function () {
+    console.log("initialise with " + user.client_id);
+    ko.applyBindings(new SubmissionsReportViewModel(user.client_id), document.getElementById("submissions"));
+});
--- src/routes/portal.js
+++ src/routes/portal.js
@@ -1,6 +1,26 @@
+var moment = require('moment');
 var express = require('express');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	
+  if (req.user) {
+    req.user.trial = { on_trial: false };
+    
+    var registered = moment(req.user._json.created_at);
+  
+    if (!req.user._json.app_metadata.subscribed_at) {
+      var now = moment(new Date());
+      
+      req.user.trial.on_trial = true;
+      req.user.trial.days_remaining = 30 - now.diff(registered, 'days');
+    }
+  }
+
+	next();
+});
 
 /* GET user profile. */
 router.get('/', ensureLoggedIn, function(req, res, next) {
